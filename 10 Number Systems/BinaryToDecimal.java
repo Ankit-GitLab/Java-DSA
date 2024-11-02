7f8141d8d1a4deb368b27d7040f7e9410f4bb9c8
Ankit-GitLab <ankit_246003@saitm.ac.in>
2024-11-02 10:29:56 +0530

    Source Code

--- 10 Number Systems/BinaryToDecimal.java
+++ 10 Number Systems/BinaryToDecimal.java
@@ -1,5 +1,7 @@
 
 
 public class BinaryToDecimal {
-    
+    public static void main(String[] args) {
+        
+    }
 }
--- 10 Number Systems/DecimalToBinary.java
+++ 10 Number Systems/DecimalToBinary.java
@@ -1,4 +1,6 @@
 
 public class DecimalToBinary {
-    
+    public static void main(String[] args) {
+        
+    }
 }
